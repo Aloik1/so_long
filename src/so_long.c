ed491edc9fd5077279901dbbfd73e68f29e9d85f
Aloik1 <ilykond@gmail.com>
2024-11-20 17:42:22 +0100

    main looks good now

--- src/so_long.c
+++ src/so_long.c
@@ -12,8 +12,74 @@
 
 #include "../includes/so_long.h"
 
-// FIX THE COLLECTIBLES NOT DISSAPPEARING< COLLECTIBLE POSITION IS WRONG FOR SOME REASON
+static int	initial_checks(int argc, char **argv, t_game *game)
+{
+	if (!game->player || !game || !game->window)
+	{
+		ft_printerror("Error: Could not allocate memory for player, game or textures\n");
+		return (0);
+	}
+	if (!basic_check(argc, argv))
+	{
+		free(game);
+		return (0);
+	}
+	if (!read_map(argv[1], game))
+	{
+		free(game);
+		return (0);
+	}
+	if (!path_checks(game))
+	{
+		ft_printerror("Error: Invalid path, can't reach all collectibles or exit\n");
+		free(game);
+		return (0);
+	}
+	return (1);
+}
 
+static int	initialize_everything(t_game *game)
+{
+	if (!window_and_mlx(&game->window))
+	{
+		free(game);
+		return (0);
+	}
+	ft_printf("Initialized window\n");
+	if (!texture_initialize(game, game->window->mlx))
+	{
+		free(game);
+		return (0);
+	}
+	game->camera = (t_camera *)malloc(sizeof(t_camera));
+	if (!game->camera)
+	{
+		free(game);
+		return (0);
+	}
+	if (!init_player(game))
+	{
+		free(game);
+		return (0);
+	}
+	camera_init(game->camera, game->player, game->map);
+	return (1);
+}
+
+static int	draw_everything(t_game *game)
+{
+	if (!draw_map(game, game->window, game->camera, game->textures))
+	{
+		free(game);
+		return (0);
+	}
+	if (!draw_player(game, game->window, game->player, game->camera))
+	{
+		free(game);
+		return (0);
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -26,82 +92,14 @@ int	main(int argc, char **argv)
 	game->player = (t_player *)malloc(sizeof(t_player));
 	game->window = (t_window *)malloc(sizeof(t_window));
 
-
-	if (!game->player || !game || !game->window)
-	{
-		ft_printerror("Error: Could not allocate memory for player, game or textures\n");
-		return (1);
-	}
-	if (!basic_check(argc, argv))
-	{
-		free(game);
-		return (1);
-	}
-	if (!read_map(argv[1], game))
-	{
-		free(game);
-		return (1);
-	}
-	if (!path_checks(game))
-	{
-		ft_printerror("Error: Invalid path, can't reach all collectibles or exit\n");
-		free(game);
-		return (1);
-	}
-	ft_printf("all checks done\n");
-
-	// create the window
-	if (!window_and_mlx(&game->window))
-	{
-		free(game);
-		ft_printf("Couldn't load window\n");
-		return (1);
-	}
-	ft_printf("Initialized window\n");
-	// initialize the textures
-	if (!texture_initialize(game, game->window->mlx))
-	{
-		ft_printf("couldn't load textures\n");
-		free(game);
-		return (1);
-	}
-	ft_printf("Collectible position in main is: %d %d\n", game->textures->collectible_1_x, game->textures->collectible_1_y);
-
-
-	// Initialize the camera
-	ft_printf("Initialized textures\n");
-	game->camera = (t_camera *)malloc(sizeof(t_camera));
-	if (!game->camera)
-	{
-		ft_printerror("Error: Could not allocate memory for camera\n");
-		free(game);
-		return (1);
-	}
-	// init the player
-	if (!init_player(game))
-	{
-		free(game);
-		return (1);
-	}
-	// initialize the camera
-	camera_init(game->camera, game->player, game->map);
-	
-	// draw the map
-	if (!draw_map(game, game->window, game->camera, game->textures))
-	{
-		free(game);
-		return (1);
-	}
-	// draw the player
-	if (!draw_player(game, game->window, game->player, game->camera))
-	{
-		free(game);
-		return (1);
-	}
-	// check for key presses
+	if (!initial_checks(argc, argv, game))
+		return (0);
+	if (!initialize_everything(game))
+		return (0);
+	if (!draw_everything(game))
+		return (0);
 	mlx_key_hook(game->window->win, movement, game);
 	mlx_hook(game->window->win, 17, 0, clean_exit, game);
-	
 	// start the loop
 	mlx_loop(game->window->mlx);
 	// Free resources before exiting
